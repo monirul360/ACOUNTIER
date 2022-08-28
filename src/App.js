d023d9bcca4c39b64029eaf7c96b4fb731ea20fe
monirul360 <monirulislam65361@gmail.com>
2022-08-28 19:19:27 +0600

    scroll problem solv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './Component/Home/Home';
 import Footer from './Component/Share/Footer/Footer';
 import Nav from './Component/Share/Nav/Nav';
@@ -35,54 +35,64 @@ import BusinessTaxFiling from './Component/BusinessTaxFiling/BusinessTaxFiling';
 import ResetPassword from './Component/ResetPassword/ResetPassword';
 import BlogsGrid from './Component/BlogsGrid/BlogsGrid';
 import BlogDetails from './Component/BlogDetails/BlogDetails';
+import { useLayoutEffect } from 'react';
 function App() {
+  const Wrapper = ({ children }) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+  }
   return (
     <>
       <Nav></Nav>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/signup' element={<Signup></Signup>}></Route>
-        <Route path='/resetpassword' element={<ResetPassword></ResetPassword>}></Route>
-        <Route path='/blogsgrid' element={<BlogsGrid></BlogsGrid>}></Route>
-        <Route path='/blogdetails' element={<BlogDetails></BlogDetails>}></Route>
-        <Route path='/aboutus' element={<Aboutus></Aboutus>}></Route>
-        <Route path='/service' element={<Service></Service>}></Route>
-        <Route path='/nidhicompany' element={<NidhiCompany></NidhiCompany>}></Route>
-        <Route path='/Producercompany' element={<Producercompany></Producercompany>}></Route>
-        <Route path='/PatentRegistration' element={<PatentRegistration></PatentRegistration>}></Route>
-        <Route path='/copyrightregistration' element={<CopyrightRegistration></CopyrightRegistration>}></Route>
-        <Route path='/TrademarkRegistration' element={<TrademarkRegistration></TrademarkRegistration>}></Route>
-        <Route path='/DigitalSignature' element={<DigitalSignature></DigitalSignature>}></Route>
-        <Route path='/FssaiRegistration' element={<FssaiRegistration></FssaiRegistration>}></Route>
-        <Route path='/ImportExportCode' element={<ImportExportCode></ImportExportCode>}></Route>
-        <Route path='/PartnershipAnnualComplianceTaxFiling'
-          element={
-            <PartnershipAnnualComplianceTaxFiling></PartnershipAnnualComplianceTaxFiling>
-          }>
+      <Wrapper>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/signup' element={<Signup></Signup>}></Route>
+          <Route path='/resetpassword' element={<ResetPassword></ResetPassword>}></Route>
+          <Route path='/blogsgrid' element={<BlogsGrid></BlogsGrid>}></Route>
+          <Route path='/blogdetails' element={<BlogDetails></BlogDetails>}></Route>
+          <Route path='/aboutus' element={<Aboutus></Aboutus>}></Route>
+          <Route path='/service' element={<Service></Service>}></Route>
+          <Route path='/nidhicompany' element={<NidhiCompany></NidhiCompany>}></Route>
+          <Route path='/Producercompany' element={<Producercompany></Producercompany>}></Route>
+          <Route path='/PatentRegistration' element={<PatentRegistration></PatentRegistration>}></Route>
+          <Route path='/copyrightregistration' element={<CopyrightRegistration></CopyrightRegistration>}></Route>
+          <Route path='/TrademarkRegistration' element={<TrademarkRegistration></TrademarkRegistration>}></Route>
+          <Route path='/DigitalSignature' element={<DigitalSignature></DigitalSignature>}></Route>
+          <Route path='/FssaiRegistration' element={<FssaiRegistration></FssaiRegistration>}></Route>
+          <Route path='/ImportExportCode' element={<ImportExportCode></ImportExportCode>}></Route>
+          <Route path='/PartnershipAnnualComplianceTaxFiling'
+            element={
+              <PartnershipAnnualComplianceTaxFiling></PartnershipAnnualComplianceTaxFiling>
+            }>
 
-        </Route>
-        <Route path='/BookkeepingServices' element={<BookkeepingServices></BookkeepingServices>}></Route>
-        <Route path='/CfoServices' element={<CfoServices></CfoServices>}></Route>
-        <Route path='/PayrollServices' element={<PayrollServices></PayrollServices>}></Route>
-        <Route path='/ContactUs' element={<ContactUs></ContactUs>}></Route>
-        <Route path='/AccountingServices' element={<AccountingServices></AccountingServices>}></Route>
-        <Route path='/IncomeTaxFiling' element={<IncomeTaxFiling></IncomeTaxFiling>}></Route>
-        <Route path='/PrivateLimitedCompanyTaxFiling'
-          element={
-            <PrivateLimitedCompanyTaxFiling></PrivateLimitedCompanyTaxFiling>
-          }>
-        </Route>
-        <Route path='/LimitedLiabilityCompany' element={<LimitedLiabilityCompany></LimitedLiabilityCompany>}></Route>
-        <Route path='/OnePersonCompanyRegistration' element={<OnePersonCompanyRegistration></OnePersonCompanyRegistration>}></Route>
-        <Route path='/IndianSubsidiary' element={<IndianSubsidiary></IndianSubsidiary>}></Route>
-        <Route path='/ProprietorshipTaxFiling' element={<ProprietorshipTaxFiling></ProprietorshipTaxFiling>}></Route>
-        <Route path='/PrivateLimitedCompanyRegistration' element={<PrivateLimitedCompanyRegistration></PrivateLimitedCompanyRegistration>}></Route>
-        <Route path='/ProprietorshipFirm' element={<ProprietorshipFirm></ProprietorshipFirm>}></Route>
-        <Route path='/PartnershipCompany' element={<PartnershipCompany></PartnershipCompany>}></Route>
-        <Route path='/BusinessTaxFiling' element={<BusinessTaxFiling></BusinessTaxFiling>}></Route>
-        <Route path='*' element={<Error></Error>}></Route>
-      </Routes>
+          </Route>
+          <Route path='/BookkeepingServices' element={<BookkeepingServices></BookkeepingServices>}></Route>
+          <Route path='/CfoServices' element={<CfoServices></CfoServices>}></Route>
+          <Route path='/PayrollServices' element={<PayrollServices></PayrollServices>}></Route>
+          <Route path='/ContactUs' element={<ContactUs></ContactUs>}></Route>
+          <Route path='/AccountingServices' element={<AccountingServices></AccountingServices>}></Route>
+          <Route path='/IncomeTaxFiling' element={<IncomeTaxFiling></IncomeTaxFiling>}></Route>
+          <Route path='/PrivateLimitedCompanyTaxFiling'
+            element={
+              <PrivateLimitedCompanyTaxFiling></PrivateLimitedCompanyTaxFiling>
+            }>
+          </Route>
+          <Route path='/LimitedLiabilityCompany' element={<LimitedLiabilityCompany></LimitedLiabilityCompany>}></Route>
+          <Route path='/OnePersonCompanyRegistration' element={<OnePersonCompanyRegistration></OnePersonCompanyRegistration>}></Route>
+          <Route path='/IndianSubsidiary' element={<IndianSubsidiary></IndianSubsidiary>}></Route>
+          <Route path='/ProprietorshipTaxFiling' element={<ProprietorshipTaxFiling></ProprietorshipTaxFiling>}></Route>
+          <Route path='/PrivateLimitedCompanyRegistration' element={<PrivateLimitedCompanyRegistration></PrivateLimitedCompanyRegistration>}></Route>
+          <Route path='/ProprietorshipFirm' element={<ProprietorshipFirm></ProprietorshipFirm>}></Route>
+          <Route path='/PartnershipCompany' element={<PartnershipCompany></PartnershipCompany>}></Route>
+          <Route path='/BusinessTaxFiling' element={<BusinessTaxFiling></BusinessTaxFiling>}></Route>
+          <Route path='*' element={<Error></Error>}></Route>
+        </Routes>
+      </Wrapper>
       <Footer></Footer>
     </>
   );
